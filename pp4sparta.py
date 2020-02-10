f4d6f732aafd1d90f22231490164af5a4da6bbe2
kenzo@Mi <luojian@mail.ustc.edu.cn>
2020-02-10 12:01:35 +0800

    add pp4sparta.py

---
+++ pp4sparta.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# pp4sparta.py
+# psotprocessing tool for sparta output data
+# 1. resave output data as tecplot flie
+# 2. compress file size
+# 3. surface pressure, shear stress and heat flux (unfinished)
+# Kenzo LUO
+# 2020.02.10
+
+import numpy as np
+import os
+import random
+import argparse
+import matplotlib.pyplot as plt
+import sys
+
+
+# error message
+def error(str):
+    if str: print("ERROR:", str)
+    else: print("Syntax: pp4sparta.py infile headfile outfile keyword args ...")
+    sys.exit()
+
+
+# read_data
+def read_data(file_path,frac,cut):
+    grid_list = []
+    with open(file_path, 'r') as f:
+        lines_list = f.readlines()
+        for lines in lines_list[9:]:
+            lines = lines.strip(' \n').split(' ')
+            for idx, line in enumerate(lines):
+                if (line == 'nan') | (line == '-nan'):
+                    lines[idx] = 0
+            lines = list(map(float, lines))
+            if cut == -1: # do not cut flow box
+                if random.random() < frac:
+                    grid_list.append(lines)
+                else:
+                    continue
+            else:
+                if (lines[cut-1] > 1e-6) & (random.random() < frac):
+                    grid_list.append(lines)
+                else:
+                    continue
+    return grid_list
+
+
+def resave_data(grid_list,headfile,outfile):
+    with open(headfile, 'r', encoding='utf-8') as hf:
+        head_text = hf.readlines()[0]
+        # print(head_text)
+    with open(outfile, 'w') as f:
+        # tecplot data file
+        f.write('TITLE ="sparta_data"\n')
+        f.write(head_text)
+        for idx in range(len(grid_list)):
+            each_data = map(str, grid_list[idx])
+            f.write(' '.join(tuple(each_data)) + '\n')
+        print("sparta data '%s' resaved. " %(outfile))
+
+
+if __name__ == '__main__':
+    arg = sys.argv
+    narg = len(sys.argv)
+    if narg < 4: error("")
+    infile = arg[1]
+    headflie = arg[2]
+    outfile = arg[3]
+
+    frac = 1
+    cut = -1
+
+    iarg = 4
+    while iarg < narg:
+        if arg[iarg] == "frac":
+            if iarg + 2 > narg: error("frac not be idenfied ")
+            frac = float(arg[iarg + 1])
+            iarg += 2
+        elif arg[iarg] == "cut":
+            if iarg + 2 > narg: error("cut_num not be idenfied ")
+            cut = int(arg[iarg + 1])
+            iarg += 2
+        else: error("")
+
+    # data reconstruction
+    print("Post-processing start...")
+    print("Data read-in start...")
+    grid_list = read_data(infile,frac,cut)
+    print("Data is resaving...")
+    resave_data(grid_list,headflie,outfile)
+    print("Post-processing complete!")
+
---
+++ readme_pp4sparta.md
@@ -0,0 +1,33 @@
+# pp4sparta后处理脚本简介
+
+## 语法
+
+### 基本语法
+
+```bash
+pp4sparta.py infile headfile outfile keyword args ...
+```
+
+`keyword`当前支持：
+
+>1. `frac` 按照一定比例压缩输出文件。例如`frac 0.5`，即压缩文件为原始文件的一半大小。（原理，随机删除一定比例的粒子）
+>2. `cut` 裁剪掉box中不在流场中的部分。由于sparta导出的grid信息是包含流场box的所有部分，有时为了减小计算量，会在来流出添加一个合适的入口边界surf，从而使得输出文件中有部分区域不在流场，但仍会被输出，各项流动信息都是0，属于无效的数据，可以将网格内的粒子数密度作为判据，网格内粒子数密度小于1e-6，则删除这一网格信息。`cut num`即裁剪掉这部分区域，且指明粒子数密度n为第`num`列，默认不进行此操作
+
+### 举例
+
+```bash
+pp4sparta.py grid24000 head.txt grid_resaved.plt cut 3 frac 0.3
+```
+
+表示处理grid24000文件，输出文件中各列的含义在head.txt的文件的第一行提供，将计算box中没有粒子分布的区域裁剪掉，表示粒子数密度的列是第三列。随机删除70%的粒子，使文件大小压缩到原来的30%。将处理后的文件另存为grid_resaved.plt
+
+### headfile
+
+headfile主要提供tecplot文件的头信息，后处理程序只读取第一行，其余行的内容可以留着备用。
+
+```text
+VARIABLES= x y n den u v Trot Tvib Ttra p MFP Kncell
+___ the rest lines will not been read ___
+VARIABLES= id x y z n den u v w Ttra p
+......
+```
